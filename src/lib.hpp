7099fafc38ec236eecbcfe2d301042836a8cde5d
Michal Walenciak <Kicer86@gmail.com>
2012-12-22 14:14:37 +0100

    lib: operator new introduced
    Former-commit-id: 0319aa4e7c7bc79c3ab00bb1b6f83e4eee57ff03 [formerly 33fbc7da10978456dbf56bf67dc9fc7a1e155cb8]
    Former-commit-id: 7553b1d1bfc7daccda60780a11924ba5d96fef07

--- src/lib.hpp
+++ src/lib.hpp
@@ -6,9 +6,21 @@
 
 int main() __attribute__((noreturn));
 extern "C" void atexit( void ) {}
-extern "C" void __cxa_pure_virtual() {}
+extern "C" void __cxa_pure_virtual() { for(;;) {} }
 extern "C" void __cxa_atexit() {}
 void operator delete(void*) {}
+
+extern "C" unsigned char _end;
+
+void* operator new(unsigned int size)
+{
+    static unsigned char *last = &_end;    
+    void *ret = last;
+    
+    last += size;
+    
+    return ret;
+}
 
 
 #if defined __AVR_ATmega8__
