aa1e8b04d63ee2e2f0f9017da3d7f10c79b74fab
Michal Walenciak <Kicer86@gmail.com>
2012-12-23 18:45:28 +0100

    one wire: initial implementation
    Former-commit-id: 46263b3c021009033d1a16170f1b12ef00ed22ea [formerly e2aa2ba27e2cfd7e71dcc192dba467bec5c0fff2]
    Former-commit-id: 06c490cbc7a2979e6e40c43369ff7bf085061589

--- src/protocols/one_wire.hpp
+++ src/protocols/one_wire.hpp
@@ -8,26 +8,78 @@
 #define __AVR_CPP_LIB__ONE_WIRE__
 
 #include <ports/ports_defs.hpp>
+#include <ports/ports.hpp>
+#include <interrupts/pcint.hpp>
+#include <delay.hpp>
 
-
-//interface for OneWire implementations
-class IOneWire
+namespace OneWire
 {
-    public:
-        virtual ~IOneWire() {}
-        virtual void init() = 0;
-};
-
-
-//factory for OneWire implementations
-struct OneWireFactory
-{
-    enum class Type
-    {
-        InterruptBased
+    template<Ports::PortT port, byte pin>
+    class InterruptBased
+    {        
+        public:
+            InterruptBased():m_state(Reset)
+            {
+                switchToInput();
+                
+                PCInt::setup(port, pin);
+            }
+                    
+            void reset()
+            {
+                PCInt::disable();
+                
+                IOPort bus(port);
+                
+                //set output to zero
+                bus[pin] = false;          //set output to zero
+                switchToOutput();
+                
+                //wait 480-960µs            
+                Delay::us<500>();
+                
+                //wait for response
+                switchToInput();
+                
+                PCInt::enable();
+            }
+            
+            void interrupt()
+            {
+                switch(m_state)
+                {
+                    case Reset:
+                        m_state = Ready;
+                        break;
+                }
+            }
+                    
+        private:        
+            enum State
+            {
+                Ready,
+                Reset
+            } m_state;
+            
+            inline void switchToOutput()
+            {
+                IOPort bus(port);
+                bus.dir[pin] = true;       //switch to output
+            }
+            
+            inline void switchToInput()
+            {
+                IOPort bus(port);
+                bus.dir[pin] = false;      //switch to input
+            }
+            
+            
+            inline void write(bool value)
+            {
+                IOPort bus(port);
+                bus.dir[pin] = false;      //switch to input
+            }
+        
     };
-    
-    static IOneWire *getOneWire(OneWireFactory::Type type, const Ports::PortT &, byte pin);
-};
-
+}
 #endif
