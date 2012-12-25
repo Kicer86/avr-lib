50c9c704296dbc18eaf3c1394e0557b6101bab32
Michal Walenciak <Kicer86@gmail.com>
2012-12-25 09:36:31 +0100

    ds18b20: primary implementation of readRawTempa
    function which will be used for acquiring temperature
    
    
    Former-commit-id: f311a7311bbd76efd184d311614cec688d53cee5 [formerly 3f28499af4b575a377a5f4ec5aeb2bd79eade62a]
    Former-commit-id: 77c62d52158b23920ca80e4a8e6a08627a8706aa

--- src/hardware/thermometers/ds18b20.hpp
+++ src/hardware/thermometers/ds18b20.hpp
@@ -2,16 +2,25 @@
 #ifndef __AVR_CPP_LIB__DS18B20__
 #define __AVR_CPP_LIB__DS18B20__
 
+/***************************************
+ * DS18B20 communiaction functionality *
+ * 25.12.2012 Michał Walenciak         *
+ ***************************************/
+
 #include "ports/ports.hpp"
 #include "protocols/one_wire.hpp"
 
 template<Ports::PortT port, byte pin>
 class DS18B20
 {
+        const byte readRomCmd = 0x33;
+        const byte convertCmd = 0x44;
+        const byte readScratchpadCmd = 0xBE;
+    
     public:
         DS18B20() 
         {
-            static_assert(sizeof(Data), "Size of Data != 8");            
+            static_assert(sizeof(Data) == 8, "Size of Data != 8");            
         }
         
         struct __attribute__((packed)) Data
@@ -21,18 +30,52 @@ class DS18B20
             byte m_crc;
         };
         
+        //detect sensor - current implementation supports only one slave        
         void init()
         {            
             byte *buffer = reinterpret_cast<byte *>(&m_data);
             
             m_oneWire.reset();
-            m_oneWire.write(0x33);
+            m_oneWire.write(readRomCmd);
             m_oneWire.read(sizeof(Data), buffer); 
         }
         
+        //get info about detected sensor
         const Data& getInfo() const
         {
             return m_data;
+        }
+        
+        word readRawTemp() const
+        {
+            m_oneWire.write(convertCmd);
+            
+            //TODO: defferent reactions in parasite/normal powering modes
+            
+            //normal powering:            
+            while(m_oneWire.read() == false);
+            //
+            
+            struct __attribute__((packed)) Scratchpad
+            {
+                byte t_lsb;
+                byte t_msb;
+                byte th;
+                byte tl;
+                byte config;
+                byte res1;
+                byte res2;
+                byte res3;
+                byte crc;
+            } scratchpad;
+            
+            static_assert(sizeof(Scratchpad) == 9, "invalid size of scratchpad structure");
+            byte *buffer = reinterpret_cast<byte *>(&scratchpad);
+            
+            m_oneWire.write(readScratchpadCmd);   
+            m_oneWire.read(sizeof(Scratchpad), buffer);
+            
+            return scratchpad.t_lsb + 256 * scratchpad.t_msb;
         }
         
     private:
