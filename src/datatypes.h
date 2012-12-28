5e4118f754f0594dbebd63c1ccd24f45836061ec
Michal Walenciak <Kicer86@gmail.com>
2012-12-28 23:14:53 +0100

    datatypes: ptr_t introduced
    Former-commit-id: 77bafb2a796b80f56227bf9462b5b6753d3f89ac [formerly 485e89b50220d5871d03a25516887d8cc2e57a3a]
    Former-commit-id: fff7ca738d29c6b496647bb0e2cd58af79d06090

--- src/datatypes.h
+++ src/datatypes.h
@@ -2,9 +2,11 @@
 #define DATATYPES_H
 
 #include <inttypes.h>
+#include <stddef.h>
 
 typedef uint8_t byte;
 typedef uint16_t word;
 typedef uint32_t dword;
+typedef size_t ptr_t;
 
 #endif //DATATYPES_H
